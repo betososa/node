57897d75d8370774d4462be63b7f77946c7e830d
Roberto Sosa <rsosa@yappme.com>
2015-02-18 08:41:33 -0600

    tidy up a little bit, a smaller callback hell

--- daemon/app.js
+++ daemon/app.js
@@ -6,19 +6,31 @@ var request = require('request');
 var CronJob = require('cron').CronJob;
 
 
-new CronJob('* * * * * *', function(){
-MongoClient.connect( 'mongodb://127.0.0.1:27017/finance', function( err, db ) {
-	if ( err ) throw err;
+// Callback to make request to finance ws
+function onConnect(db){
 	var collection = db.collection( 'stocks' );
 	request('http://finance.yahoo.com/webservice/v1/symbols/AAPL,%5EDJI,GOOGL,%5EIXIC/quote?format=json', function (error, response, body){
-		if ( !error && response.statusCode == 200 ){
-			console.log('en request');
-			body = JSON.parse(body);
-			collection.insert(body, function(error, docs) {
-				if (error) throw error;
-				db.close();
-			});
-		}
-	})
+		if (error) throw error;
+		// we made a request, no errors
+		onRequest(body, collection, db);
+	});
+}
+
+// we have db, collection and request, now go and save it!
+function onRequest(body, collection, db){
+	body = JSON.parse(body);
+	collection.insert(body, function(error, docs){
+		if (error) throw error;
+		db.close()
+	});
+}
+
+// CronJob for executing requests every second
+new CronJob('* * * * * *', function(){
+	console.log('Aqui tambien');
+MongoClient.connect( 'mongodb://127.0.0.1:27017/finance', function( err, db ) {
+	if ( err ) throw err;
+	// We have a conenction
+	onConnect(db);
 })
 }, null, true, "America/Los_Angeles");
